6fbe88a58a07a6be9eaf82b6529639fcfd5a4507
Deep <sonideep903@gmail.com>
2025-09-03 15:41:50 +0530

    Delete src/main directory

--- src/main/java/com/safvan/repository/restapi/IApiTicketRespository.java
+++
@@ -1,9 +0,0 @@
-package com.safvan.repository.restapi;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.safvan.beans.restapi.ApiTicket;
-
-public interface IApiTicketRespository extends PagingAndSortingRepository<ApiTicket, String> {
-
-}
